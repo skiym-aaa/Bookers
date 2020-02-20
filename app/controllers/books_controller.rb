d917d3560824e1181b1f474b416e70d8dd13e817
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-20 13:25:58 +0900

    [Add] Editing screen

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,11 +16,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      book = Book.find(params[:id])
+      book.update(book_params)
+      redirect_to book_path(book)
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :body)
   end
 
 end
